a0c015993a31f35095f791c082f2e457f76076ca
Galim Usaev <55399978+galimus@users.noreply.github.com>
2024-03-27 01:48:00 +0000

    add finding classes

--- search_code.py
+++ search_code.py
@@ -2,18 +2,23 @@ import requests
 import re
 import time
 
+# Ваш GitHub Personal Access Token
+github_token = ""
 
-github_token = "qqqqqqqq"
-
-
-def search_github_for_function_signature(function_name, function_signature):
-
+def search_github_for_code_element(name, signature="", content_type="function"):
+    """Ищет в GitHub файлы, содержащие указанные элементы кода: функции или классы."""
     headers = {
         'Authorization': f'token {github_token}',
         'Accept': 'application/vnd.github.v3.text-match+json',
     }
-   
-    query = f'"{function_name}"+"{function_signature}" in:file language:python'
+    if content_type == "function":
+        query = f'"{name}"+"{signature}" in:file language:python'
+    elif content_type == "class":
+        query = f'class "{name}" in:file language:python'
+    else:
+        print(f"Unsupported content_type: {content_type}")
+        return []
+
     url = f"https://api.github.com/search/code?q={query}"
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
@@ -23,17 +28,33 @@ def search_github_for_function_signature(function_name, function_signature):
         return []
 
 def extract_function_signatures_from_file(file_path):
-   
+    """Извлекает названия функций и их сигнатуры из файла с исходным кодом."""
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
-    pattern = re.compile(r'def (\w+)\((.*?)\):', re.DOTALL)
-    return pattern.findall(content)
+    return re.findall(r'def (\w+)\((.*?)\):', content, re.DOTALL)
 
-local_file_path = '/path'
+def extract_classes_from_file(file_path):
+    """Извлекает названия классов и их содержимое из файла с исходным кодом."""
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    # Используем регулярное выражение для поиска классов и сохраняем весь блок класса
+    class_pattern = re.compile(r'class (\w+)(\(.*?\))?:\s*\n((?:\s+(?:def|class|@|\w).+\n)+)', re.MULTILINE)
+    return class_pattern.findall(content)
+
+local_file_path = ''
+
+# Поиск и вывод результатов для функций
 function_signatures = extract_function_signatures_from_file(local_file_path)
-
 for function_name, function_signature in function_signatures:
-    matches = search_github_for_function_signature(function_name, function_signature)
+    matches = search_github_for_code_element(function_name, function_signature, content_type="function")
     for match in matches:
         print(f"Found {function_name} in {match['repository']['html_url']}/blob/{match['sha']}/{match['path']}")
 
+class_definitions = extract_classes_from_file(local_file_path)
+
+
+for class_name, class_args, _ in class_definitions:
+    class_matches = search_github_for_code_element(class_name, content_type="class")
+    for match in class_matches:
+        print(f"Found class {class_name} in {match['repository']['html_url']}/blob/{match['sha']}/{match['path']}")
+# Поиск и вывод результатов для классов
