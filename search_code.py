c4ec82be1f281133da64be62c4ea12c9918ab242
Galim Usaev <55399978+galimus@users.noreply.github.com>
2024-03-27 01:50:37 +0000

    Update search_code.py

--- search_code.py
+++ search_code.py
@@ -2,11 +2,11 @@ import requests
 import re
 import time
 
-# Ваш GitHub Personal Access Token
+
 github_token = ""
 
 def search_github_for_code_element(name, signature="", content_type="function"):
-    """Ищет в GitHub файлы, содержащие указанные элементы кода: функции или классы."""
+
     headers = {
         'Authorization': f'token {github_token}',
         'Accept': 'application/vnd.github.v3.text-match+json',
@@ -28,28 +28,28 @@ def search_github_for_code_element(name, signature="", content_type="function"):
         return []
 
 def extract_function_signatures_from_file(file_path):
-    """Извлекает названия функций и их сигнатуры из файла с исходным кодом."""
+
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
     return re.findall(r'def (\w+)\((.*?)\):', content, re.DOTALL)
 
 def extract_classes_from_file(file_path):
-    """Извлекает названия классов и их содержимое из файла с исходным кодом."""
+    
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
-    # Используем регулярное выражение для поиска классов и сохраняем весь блок класса
+
     class_pattern = re.compile(r'class (\w+)(\(.*?\))?:\s*\n((?:\s+(?:def|class|@|\w).+\n)+)', re.MULTILINE)
     return class_pattern.findall(content)
 
 local_file_path = ''
 
-# Поиск и вывод результатов для функций
+
 function_signatures = extract_function_signatures_from_file(local_file_path)
 for function_name, function_signature in function_signatures:
     matches = search_github_for_code_element(function_name, function_signature, content_type="function")
     for match in matches:
         print(f"Found {function_name} in {match['repository']['html_url']}/blob/{match['sha']}/{match['path']}")
-
+    time.sleep(1)
 class_definitions = extract_classes_from_file(local_file_path)
 
 
@@ -57,4 +57,4 @@ for class_name, class_args, _ in class_definitions:
     class_matches = search_github_for_code_element(class_name, content_type="class")
     for match in class_matches:
         print(f"Found class {class_name} in {match['repository']['html_url']}/blob/{match['sha']}/{match['path']}")
-# Поиск и вывод результатов для классов
+    time.sleep(1)
